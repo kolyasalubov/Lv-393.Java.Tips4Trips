8762e2d3e9323c23b5fe639c3cc053d1b1621943
Roman Boiarchuk <r.s.boyarchuk@gmail.com>
2019-04-05 19:22:47 +0300

    created generic Service

--- server/src/main/java/com/softserve/academy/Tips4Trips/service/Service.java
+++ server/src/main/java/com/softserve/academy/Tips4Trips/service/Service.java
@@ -2,6 +2,15 @@ package com.softserve.academy.Tips4Trips.service;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Service<T, ID, R extends JpaRepository<T,ID>> {
+import java.util.List;
+import java.util.Optional;
 
+public interface Service<T, ID, R extends JpaRepository<T, ID>> {
+    List<T> findAll();
+
+    Optional<T> findById(ID id);
+
+    T save(T entity);
+
+    void delete(T entity);
 }
--- server/src/main/java/com/softserve/academy/Tips4Trips/service/impl/ServiceIml.java
+++ server/src/main/java/com/softserve/academy/Tips4Trips/service/impl/ServiceIml.java
@@ -3,7 +3,30 @@ package com.softserve.academy.Tips4Trips.service.impl;
 import com.softserve.academy.Tips4Trips.service.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 @org.springframework.stereotype.Service
 public class ServiceIml<T,ID,R extends JpaRepository<T,ID>> implements Service<T,ID,R> {
+    protected R repository;
 
+    @Override
+    public List<T> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Optional<T> findById(ID id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public T save(T entity) {
+        return repository.save(entity);
+    }
+
+    @Override
+    public void delete(T entity) {
+        repository.delete(entity);
+    }
 }
