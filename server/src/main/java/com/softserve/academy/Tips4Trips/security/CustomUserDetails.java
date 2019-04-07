e5153360649cd6e18e07401fd91aa8076e1e1689
TsiupakOlena <ts.lenius@gmail.com>
2019-04-07 22:09:00 +0300

    Added some security classes

---
+++ server/src/main/java/com/softserve/academy/Tips4Trips/security/CustomUserDetails.java
@@ -0,0 +1,119 @@
+package com.softserve.academy.Tips4Trips.security;
+
+
+import com.softserve.academy.Tips4Trips.entity.Role;
+import com.softserve.academy.Tips4Trips.entity.User;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+import static com.softserve.academy.Tips4Trips.entity.Role.ADMIN;
+
+public class CustomUserDetails implements UserDetails {
+    private Long id;
+
+    @JsonIgnore
+    private String login;
+
+    @JsonIgnore
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(Long id, String login, String password,
+                             Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public static CustomUserDetails create(User user, Role role) {
+        List<GrantedAuthority> authorities =new ArrayList<>();
+
+        switch(role) {
+            case ADMIN:
+                authorities.add(new SimpleGrantedAuthority(
+                        RoleConstant.ROLE_ADMIN)
+                );
+                /*falls through*/
+            case MODERATOR:
+                authorities.add(new SimpleGrantedAuthority(
+                        RoleConstant.ROLE_MODERATOR)
+                );
+                /*falls through*/
+            case USER:
+                authorities.add(new SimpleGrantedAuthority(
+                        RoleConstant.ROLE_USER)
+                );
+
+        }
+
+        return new CustomUserDetails(
+                user.getId(),
+                user.getLogin(),
+                user.getPassword(),
+                authorities
+        );
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CustomUserDetails that = (CustomUserDetails) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+}
---
+++ server/src/main/java/com/softserve/academy/Tips4Trips/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package com.softserve.academy.Tips4Trips.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest,
+                         HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException, ServletException {
+        System.out.println("Responding with unauthorized error. Message - " + e.getMessage());
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+    }
+}
