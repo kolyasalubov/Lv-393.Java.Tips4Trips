60a5bb3b8a325c46c12712418405537e84b8d4f7
proonk <proonk@gmail.com>
2019-04-10 13:49:24 +0300

    restaurant controller

--- server/src/main/java/com/softserve/academy/Tips4Trips/controller/RestaurantController.java
+++ server/src/main/java/com/softserve/academy/Tips4Trips/controller/RestaurantController.java
@@ -2,44 +2,47 @@ package com.softserve.academy.Tips4Trips.controller;
 
 import com.softserve.academy.Tips4Trips.dto.RestaurantDTO;
 import com.softserve.academy.Tips4Trips.dto.converter.RestaurantConverter;
-import com.softserve.academy.Tips4Trips.entity.Position;
-import com.softserve.academy.Tips4Trips.entity.enums.TypeOfBuilding;
-import com.softserve.academy.Tips4Trips.repository.RestaurantRepository;
+import com.softserve.academy.Tips4Trips.entity.place.Restaurant;
+import com.softserve.academy.Tips4Trips.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.DayOfWeek;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
+import java.util.List;
 
-@RestController("/restaurants")
+@RestController
+@CrossOrigin
+@RequestMapping("/restaurants")
 public class RestaurantController {
 
+    private RestaurantService restaurantService;
+    private RestaurantConverter restaurantConverter;
+
     @Autowired
-    RestaurantRepository restaurantRepository;
-    @Autowired
-    RestaurantConverter restaurantConverter;
+    public RestaurantController(RestaurantService restaurantService, RestaurantConverter restaurantConverter) {
+        this.restaurantService = restaurantService;
+        this.restaurantConverter = restaurantConverter;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<RestaurantDTO>> getAll(){
+        return new ResponseEntity<>(restaurantConverter.convertToDTO(restaurantService.findAll()), HttpStatus.OK);
+    }
 
     @PostMapping("/create")
-    public void create(){
-        RestaurantDTO restaurantDTO = new RestaurantDTO();
-        restaurantDTO.setHasVeganFood(true);
-        restaurantDTO.setAverageBill(15.2F);
-        restaurantDTO.setAddress("asd");
-        restaurantDTO.setCityId((long) 1);
-        restaurantDTO.setClosingTime(new Date());
-        restaurantDTO.setOpeningTime(new Date());
-        restaurantDTO.setName("asd");
-        restaurantDTO.setPhotoPath("asd");
-        restaurantDTO.setPosition(new Position());
-        restaurantDTO.setWebSite("asdasd");
-        restaurantDTO.setDescription("asdas");
-        restaurantDTO.setTelephone("fvgbhjk");
-        restaurantDTO.setType(TypeOfBuilding.ENTERTAINMENT);
-        restaurantDTO.setWorkingDays(new ArrayList<DayOfWeek>(Collections.singleton(DayOfWeek.MONDAY)));
+    public Restaurant createRestaurant(@RequestBody RestaurantDTO restaurantDTO) {
+        Restaurant restaurant = restaurantConverter.convertToEntity(restaurantDTO);
+        return restaurantService.createRestaurant(restaurant);
+    }
 
-        restaurantRepository.save(restaurantConverter.convertToEntity(restaurantDTO));
+    @GetMapping("/{id}")
+    public ResponseEntity<RestaurantDTO> getById(@PathVariable Long id) {
+        return new ResponseEntity<>(restaurantConverter.convertToDTO(restaurantService.findById(id)), HttpStatus.OK);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<Restaurant> update(@RequestBody RestaurantDTO restaurantDTO) {
+        return new ResponseEntity<>(restaurantService.update(restaurantConverter.convertToEntity(restaurantDTO)), HttpStatus.OK);
     }
 }
