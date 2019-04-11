41ca0da42b14fd0f16fd12bf0e91854b4989f18d
proonk <proonk@gmail.com>
2019-04-11 15:30:30 +0300

    ???

--- server/src/main/java/com/softserve/academy/Tips4Trips/repository/AccountRepository.java
+++ server/src/main/java/com/softserve/academy/Tips4Trips/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.softserve.academy.Tips4Trips.repository;
 
 import com.softserve.academy.Tips4Trips.entity.administration.Account;
+import com.softserve.academy.Tips4Trips.entity.administration.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
