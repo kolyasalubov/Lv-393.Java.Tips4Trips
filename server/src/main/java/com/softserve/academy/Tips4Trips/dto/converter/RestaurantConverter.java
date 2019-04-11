968b09c4c32b080c025120c6b96360f234379636
proonk <proonk@gmail.com>
2019-04-11 20:13:37 +0300

    restaurant controller changes

--- server/src/main/java/com/softserve/academy/Tips4Trips/dto/converter/RestaurantConverter.java
+++ server/src/main/java/com/softserve/academy/Tips4Trips/dto/converter/RestaurantConverter.java
@@ -2,6 +2,7 @@ package com.softserve.academy.Tips4Trips.dto.converter;
 
 import com.softserve.academy.Tips4Trips.dto.RestaurantDTO;
 import com.softserve.academy.Tips4Trips.entity.place.Restaurant;
+import com.softserve.academy.Tips4Trips.service.CityService;
 import com.softserve.academy.Tips4Trips.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -10,10 +11,12 @@ import org.springframework.stereotype.Component;
 public class RestaurantConverter implements Converter<Restaurant, RestaurantDTO> {
 
     private RestaurantService restaurantService;
+    private CityService cityService;
 
     @Autowired
-    public RestaurantConverter (RestaurantService restaurantService) {
+    public RestaurantConverter (RestaurantService restaurantService, CityService cityService) {
         this.restaurantService = restaurantService;
+        this.cityService = cityService;
     }
 
 
@@ -27,6 +30,11 @@ public class RestaurantConverter implements Converter<Restaurant, RestaurantDTO>
         restaurantDTO.setPosition(restaurant.getPosition());
         restaurantDTO.setPhotoPath(restaurant.getPhotoPath());
         restaurantDTO.setCityId(restaurant.getCity().getId());
+        restaurantDTO.setWorkingDays(restaurant.getWorkingDays());
+        restaurantDTO.setWebSite(restaurant.getWebSite());
+        restaurantDTO.setTelephone(restaurant.getTelephone());
+        restaurantDTO.setOpeningTime(restaurant.getOpeningTime());
+        restaurantDTO.setClosingTime(restaurant.getClosingTime());
         restaurantDTO.setAverageBill(restaurant.getAverageBill());
         restaurantDTO.setHasVeganFood(restaurant.getHasVeganFood());
 
@@ -42,6 +50,12 @@ public class RestaurantConverter implements Converter<Restaurant, RestaurantDTO>
         restaurant.setAddress(restaurantDTO.getAddress());
         restaurant.setPosition(restaurantDTO.getPosition());
         restaurant.setPhotoPath(restaurantDTO.getPhotoPath());
+        restaurant.setCity(cityService.findById(restaurantDTO.getCityId()));
+        restaurant.setWorkingDays(restaurantDTO.getWorkingDays());
+        restaurant.setWebSite(restaurantDTO.getWebSite());
+        restaurant.setType(restaurantDTO.getType());
+        restaurant.setOpeningTime(restaurantDTO.getOpeningTime());
+        restaurant.setClosingTime(restaurantDTO.getClosingTime());
         restaurant.setAverageBill(restaurantDTO.getAverageBill());
         restaurant.setHasVeganFood(restaurantDTO.getHasVeganFood());
         return restaurant;
