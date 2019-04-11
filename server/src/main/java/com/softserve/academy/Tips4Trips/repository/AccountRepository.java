af83c4f162dd2b7fad9ea263ce4cc3340b655243
Volodymyr <vovapaziuk@gmail.com>
2019-04-11 12:33:17 +0300

    add search by user

--- server/src/main/java/com/softserve/academy/Tips4Trips/repository/AccountRepository.java
+++ server/src/main/java/com/softserve/academy/Tips4Trips/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.softserve.academy.Tips4Trips.repository;
 
 import com.softserve.academy.Tips4Trips.entity.administration.Account;
+import com.softserve.academy.Tips4Trips.entity.administration.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,7 @@ import java.util.Optional;
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Optional<Account> findByEmail(String email);
+
+    Optional<Account> findByUsersContains(User user);
 
 }
