a83da16b3f2638bb839e5d7b8c49136c5296542b
TsiupakOlena <ts.lenius@gmail.com>
2019-04-12 20:31:51 +0300

    Added custom exceptions

---
+++ server/src/main/java/com/softserve/academy/Tips4Trips/exception/DataNotFoundException.java
@@ -0,0 +1,8 @@
+package com.softserve.academy.Tips4Trips.exception;
+
+public class DataNotFoundException extends Exception {
+    public DataNotFoundException() {}
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
