39955c73336f1ad1943cbd81388d53f4d2cab958
Volodymyr <vovapaziuk@gmail.com>
2019-04-13 18:17:24 +0300

    account DTOs

---
+++ server/src/main/java/com/softserve/academy/Tips4Trips/dto/details/AccountDetailsDTO.java
@@ -0,0 +1,59 @@
+package com.softserve.academy.Tips4Trips.dto.details;
+
+import com.softserve.academy.Tips4Trips.dto.info.AccountInfoDTO;
+import com.softserve.academy.Tips4Trips.entity.enums.Role;
+
+import java.util.Date;
+
+public class AccountDetailsDTO extends AccountInfoDTO {
+
+    private String phoneNumber;
+
+    private String email;
+
+    private Date registrationDate;
+
+    private String about;
+
+    private Role role;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
--- server/src/main/java/com/softserve/academy/Tips4Trips/dto/info/AccountInfoDTO.java
+++ server/src/main/java/com/softserve/academy/Tips4Trips/dto/info/AccountInfoDTO.java
@@ -3,11 +3,41 @@ package com.softserve.academy.Tips4Trips.dto.info;
 public class AccountInfoDTO {
     private Long id;
 
+    private String firstName;
+
+    private String lastName;
+
+    private String self;
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSelf() {
+        return self;
+    }
+
+    public void setSelf(String self) {
+        this.self = self;
     }
 }
