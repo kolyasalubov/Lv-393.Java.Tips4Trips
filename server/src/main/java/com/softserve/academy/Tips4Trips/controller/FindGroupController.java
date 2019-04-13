fb1db36a0065ff04182ce98ffb525eaf0d140b83
Volodymyr <vovapaziuk@gmail.com>
2019-04-13 20:38:56 +0300

    find group DTOs and converters

--- server/src/main/java/com/softserve/academy/Tips4Trips/controller/FindGroupController.java
+++ server/src/main/java/com/softserve/academy/Tips4Trips/controller/FindGroupController.java
@@ -2,12 +2,16 @@ package com.softserve.academy.Tips4Trips.controller;
 
 import com.softserve.academy.Tips4Trips.dto.FindGroupDTO;
 import com.softserve.academy.Tips4Trips.dto.converter.FindGroupConverter;
+import com.softserve.academy.Tips4Trips.dto.details.FindGroupDetailsDTO;
+import com.softserve.academy.Tips4Trips.dto.info.FindGroupInfoDTO;
 import com.softserve.academy.Tips4Trips.entity.entertainment.mountains.FindGroup;
 import com.softserve.academy.Tips4Trips.service.FindGroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -24,16 +28,23 @@ public class FindGroupController {
     }
 
 
+    @GetMapping
+    public ResponseEntity<List<FindGroupInfoDTO>> getAll() {
+        return new ResponseEntity<>(findGroupConverter
+                .convertToInfoDTO(findGroupService.findAll()), HttpStatus.OK);
+    }
+
+
     @GetMapping("/{id}")
-    public ResponseEntity<FindGroupDTO> getById(@PathVariable long id) {
+    public ResponseEntity<FindGroupDetailsDTO> getById(@PathVariable Long id) {
         return new ResponseEntity<>(findGroupConverter
                 .convertToDTO(findGroupService.findById(id)), HttpStatus.OK);
     }
 
 
     @PostMapping("/create")
-    public ResponseEntity<FindGroupDTO> createPost(@RequestBody FindGroupDTO findGroupDTO) {
-        FindGroup findGroup = findGroupService.createFindGroup(findGroupConverter.convertToEntity(findGroupDTO));
+    public ResponseEntity<FindGroupDetailsDTO> createPost(@RequestBody FindGroupDetailsDTO findGroupDetailsDTO) {
+        FindGroup findGroup = findGroupService.createFindGroup(findGroupConverter.convertToEntity(findGroupDetailsDTO));
         return new ResponseEntity<>(findGroupConverter.convertToDTO(findGroup), HttpStatus.CREATED);
 
     }
