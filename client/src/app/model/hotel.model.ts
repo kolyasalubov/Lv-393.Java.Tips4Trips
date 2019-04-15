d697963aed83208048f3f919af4400e136b59eb5
Volodymyr <vovapaziuk@gmail.com>
2019-04-15 20:31:25 +0300

    hotel.model

--- client/src/app/model/hotel.model.ts
+++ client/src/app/model/hotel.model.ts
@@ -1,21 +1,36 @@
+import{Position} from "./position.model"
+import{City} from "./city.model"
 export class Hotel {
 
 
- //List<DayOfWeek> workingDays;
+ workingDays:string [];
  webSite: string;
  telephone: string;
-//TypeOfBuilding type;
+ typeOfBuilding: string;
  openingTime: Date;
  closingTime: Date;
  address: string;
- //Position position;
+ position: Position;
  photoPath: string;
- //CityDTO cityDTO;
+ city: City ;
  maximumPrice : number;
  minimumPrice : number;
 
 
-    constructor( ) {
-      
+    constructor( workingDays:string [],webSite: string,telephone: string,typeOfBuilding: string,openingTime: Date,closingTime: Date,address: string,position: Position,photoPath: string,city: City ,maximumPrice : number,minimumPrice : number,
+       ) {
+        this.workingDays = workingDays;
+        this.webSite = webSite;
+        this.telephone = telephone;
+        this.typeOfBuilding = typeOfBuilding;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+        this.address = address;
+        this.position = position;
+        this.photoPath = photoPath;
+        this.city = city;
+        this.maximumPrice = maximumPrice;
+        this.minimumPrice = minimumPrice;
+       
       }
 }
