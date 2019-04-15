9743cfa898f2fb5eb8a69062cf50ecee857fea6e
Yurii Mota <yuriimota89@gmail.com>
2019-04-15 15:14:20 +0300

    Created Like Component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/authentication/login/login.component';
 import {CreatePostPlaceComponent} from "./components/create-post-place/create-post-place.component";
+import {LikeComponent} from "./components/blog/like/like.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'create-place', component: CreatePostPlaceComponent },
+  { path: 'like', component: LikeComponent },
 ];
 
 @NgModule({
