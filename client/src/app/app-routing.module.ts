f9c01e7a7d2f22b2a66f66ca5915f097c2db38c5
narnanam <narnanam@gmail.com>
2019-04-15 14:13:42 +0300

    url

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/authentication/login/login.component';
+import { AccountComponent } from './components/authentication/account/account.component';
+import {CreatePostComponent} from "./components/create-post/create-post.component";
 import {CreatePostPlaceComponent} from "./components/create-post-place/create-post-place.component";
+import {CreatePostRouteComponent} from "./components/create-post-route/create-post-route.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'create-place', component: CreatePostPlaceComponent },
+  { path: 'account', component: AccountComponent },
+  { path: 'create_place', component: CreatePostComponent },
+  { path: 'create_post', component: CreatePostPlaceComponent },
+  { path: 'create_route', component: CreatePostRouteComponent }
 ];
 
 @NgModule({
