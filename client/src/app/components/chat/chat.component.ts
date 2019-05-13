261d0e16cf69a43a990832b16518a0f8b0dd6100
Volodymyr <vovapaziuk@gmail.com>
2019-05-14 02:57:23 +0300

    chat code refactor

--- client/src/app/components/chat/chat.component.ts
+++ client/src/app/components/chat/chat.component.ts
@@ -11,7 +11,7 @@ import {CustomAuthService} from "../authentication/custom-auth.service";
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit, AfterViewChecked  {
+export class ChatComponent implements OnInit, AfterViewChecked {
 
   @Input() chatId: number;
 
@@ -21,19 +21,14 @@ export class ChatComponent implements OnInit, AfterViewChecked  {
   private stompClient;
   currentAccountId: number;
   msg: String;
-   messages: Message[] = [];
+  messages: Message[] = [];
   @ViewChild('scrollMe') private myScrollContainer: ElementRef;
 
-   colors = [
-    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
-    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
-  ];
+  chatMessageInfo: ChatMessageInfoDTO = new ChatMessageInfoDTO(null, null, null);
 
-  chatMessageInfo: ChatMessageInfoDTO = new ChatMessageInfoDTO(null,null,null);
 
-  
-  constructor(private chatService: ChatService, private authService: CustomAuthService) { }
-
+  constructor(private chatService: ChatService, private authService: CustomAuthService) {
+  }
 
   ngOnInit() {
     this.authService.getCurrentUser().subscribe(data => this.currentAccountId = data.id);
@@ -50,7 +45,9 @@ export class ChatComponent implements OnInit, AfterViewChecked  {
   scrollToBottom(): void {
     try {
       this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
-    } catch(err) { console.log(err)}
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   initializeWebSocketConnection() {
@@ -58,40 +55,31 @@ export class ChatComponent implements OnInit, AfterViewChecked  {
     this.stompClient = Stomp.over(ws);
     let that = this;
     this.stompClient.connect({}, function (frame) {
-      console.log('Connecting stomp');
-      console.log(frame);
       that.openSocket();
     });
 
   }
 
   openSocket() {
-    console.log("opening socket");
-    this.stompClient.subscribe("/topic/messages/" + this.chatId , (message) => {
-      console.log("subs to messages");
+    this.stompClient.subscribe("/topic/messages/" + this.chatId, (message) => {
       this.handleResult(message);
-      //this.onMessageReceived(message);
     });
 
   }
 
   handleResult(message) {
-    console.log("handling result");
-    console.log(message);
-    console.log(JSON.parse(message.body));
+    JSON.parse(message.body);
     if (message.body) {
       let messageResult: Message = JSON.parse(message.body);
-      console.log(messageResult);
       this.messages.push(messageResult);
-      console.log(this.messages);
     }
   }
 
   sendMessage(message) {
     if (message) {
-      this.chatMessageInfo.chatId=this.chatId;
-      this.chatMessageInfo.accountId=this.currentAccountId;
-      this.chatMessageInfo.content=message;
+      this.chatMessageInfo.chatId = this.chatId;
+      this.chatMessageInfo.accountId = this.currentAccountId;
+      this.chatMessageInfo.content = message;
       this.stompClient.send("/chat/send/message", {}, JSON.stringify(this.chatMessageInfo));
 
     }
